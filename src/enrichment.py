6260dde3db3aa1271ecd6e94838904ff376a376a
FathimaNafra <fathumanafra5@gmail.com>
2026-06-23 11:15:34 +0530

    Add occupancy rate enrichment

--- src/enrichment.py
+++ src/enrichment.py
@@ -1,68 +1,48 @@
 import pandas as pd
 
 
+def load_data():
+
+    listings = pd.read_csv(
+        "../data/processed/listings_clean.csv"
+    )
+
+    calendar = pd.read_csv(
+        "../data/processed/calendar_clean.csv"
+    )
+
+    reviews = pd.read_csv(
+        "../data/processed/reviews_clean.csv"
+    )
+
+    return listings, calendar, reviews
+
 def calculate_occupancy(calendar):
 
-    occupancy = (
-        calendar.groupby(
-            "listing_id"
-        )["available"]
-        .apply(
-            lambda x:
-            (x == "f").sum()
-            / len(x)
+    calendar["booked"] = (
+        calendar["available"]
+        .map(
+            {
+                "f": 1,
+                "t": 0
+            }
         )
+    )
+
+    occupancy = (
+        calendar
+        .groupby("listing_id")
+        ["booked"]
+        .mean()
         .reset_index()
     )
 
-    occupancy.columns = [
-        "listing_id",
-        "occupancy_rate"
-    ]
+    occupancy.rename(
+        columns={
+            "booked":
+            "occupancy_rate"
+        },
+        inplace=True
+    )
 
     return occupancy
-
-#Revenue Estimate
-def revenue_estimate(df):
-
-    df["estimated_revenue"] = (
-        df["price"]
-        * 365
-        * df["occupancy_rate"]
-    )
-
-    return df
-
-#Host Tenure
-def host_tenure(df):
-
-    current_year = 2026
-
-    df["host_tenure_years"] = (
-        current_year
-        - df["host_since"].dt.year
-    )
-
-    return df
-
-#Join Data
-occupancy = calculate_occupancy(
-    calendar
-)
-
-master = listings.merge(
-    occupancy,
-    left_on="id",
-    right_on="listing_id",
-    how="left"
-)
-
-master = revenue_estimate(master)
-
-master = host_tenure(master)
-
-
-master.to_csv(
-    "data/curated/listing_master.csv",
-    index=False
-)
